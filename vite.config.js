c0abf4e142798ec67c2eeff2a6c269530556cd8b
Kalib <kalibuas@gmail.com>
2021-06-08 17:29:57 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+const { resolve } = require('path')
+
+module.exports = {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()]
 })
+
+
